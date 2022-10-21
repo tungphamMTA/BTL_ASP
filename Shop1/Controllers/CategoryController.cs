5ce3a14b83a598f675416253179bcfb2f25d0dbf
tungphamMTA <tungphampqt@gmail.com>
2022-10-21 09:30:41 +0700

    First upload

---
+++ Shop1/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Shop1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using PagedList;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop1.Controllers
+{
+    
+    public class CategoryController : Controller
+    {
+        DBContext con = new DBContext();
+        // GET: Category
+        public class listofSegments
+        {
+            public string Text { get; set; }
+            public string Value { get; set; }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult DanhMucHSXPartial()
+        {
+            var hang_sx = con.HangSanXuat.ToList();
+            return PartialView(hang_sx);
+        }
+
+
+        public PartialViewResult SanPhamTheoDanhMuc(string id, int page = 1, int pageSize = 6)
+        {
+            var segmentList = new List<listofSegments>();
+            listofSegments segmentItem;
+            var sizeArr = new string[] { "6", "9", "12", "15" };
+            for(int idx = 0; idx < sizeArr.Length; idx++ )
+            {
+                segmentItem = new listofSegments();
+                segmentItem.Text = sizeArr[idx];
+                segmentItem.Value = sizeArr[idx];
+                segmentList.Add(segmentItem);
+            }
+            ViewBag.segmentList = segmentList;
+
+            HangSanXuat dm_HSX = con.HangSanXuat.SingleOrDefault(x => x.TenHang == id);
+            if (dm_HSX == null)
+            {
+                Console.WriteLine("tungp");
+            }
+            List<SanPham> sanphams = con.SanPham.Where(x => x.HangSX == dm_HSX.HangSX).ToList();
+            ViewBag.hangsx = id;
+            var model = sanphams.OrderByDescending(x => x.GiaGoc).ToPagedList(page, pageSize);
+            return PartialView(model);
+        }
+    }
+}
---
+++ Shop1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop1.Models;
+using Shop1.Controllers;
+using PagedList;
+
+namespace Shop1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class listofSegments
+        {
+            public string Text { get; set; }
+            public string Value { get; set; }
+        }
+        DBContext con = new DBContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Shop(int page = 1, int pageSize = 9)
+        {
+            List<SanPham> sanPham = con.SanPham.ToList();
+            var segmentList = new List<listofSegments>();
+            listofSegments segmentItem;
+            var arr = new string[] { "9", "12", "24", "36" };
+            for (int idx =0; idx< arr.Length; idx++)
+            {
+                segmentItem = new listofSegments();
+                segmentItem.Text = arr[idx];
+                segmentItem.Value = arr[idx];
+                segmentList.Add(segmentItem);
+            }
+            ViewBag.segmentList = segmentList;
+            var model = sanPham.OrderByDescending(x => x.GiaGoc).ToPagedList(page, pageSize);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult ActionPostData(string Segmentation)
+        {
+            return RedirectToAction("Shop", new { pageSize = Convert.ToInt32(Segmentation) });
+        }
+    }
+}
